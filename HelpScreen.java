037ba6ef63377407870b876658b6d37a79b94631
Erika-Bey <143749717+Erika-Bey@users.noreply.github.com>
2023-10-30 15:28:43 -0400

    Update HelpScreen.java

--- HelpScreen.java
+++ HelpScreen.java
@@ -17,5 +17,6 @@ public class HelpScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        setBackground("Backgrounds/citypopup.png");
     }
 }
