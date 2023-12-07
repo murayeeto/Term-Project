f892c101d623442bcb985162c59ee4e7907fd643
Erika B <emsuberbey23@students.desu.edu>
2023-12-07 13:01:15 -0500

    didn't have time to add achievements so I changed end screens

--- GameSummary.java
+++ GameSummary.java
@@ -21,7 +21,7 @@ public class GameSummary extends Actor
     public GameSummary(Character player){
         this.player = player;
         NameInput.loadName();
-        setImage(new GreenfootImage(NameInput.getName()+" ended with "+"$"+player.getBalance()+" and a credit score of "+player.getCredit(),24,Color.BLACK,new Color(0, 0, 0, 0)));
+        setImage(new GreenfootImage(NameInput.getName()+" ended with "+"$"+player.getBalance()+"\nand a credit score of "+player.getCredit(),24,Color.BLACK,new Color(0, 0, 0, 0)));
     }
     public GameSummary(){
         this.player = player;
